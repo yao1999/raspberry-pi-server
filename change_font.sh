2da74072110c96be7e00f54c4401e7e4950b6eb7
Zefeng Yao <36053374+yao1999@users.noreply.github.com>
2022-05-15 19:22:57 -0400

    add

--- change_font.sh
+++
@@ -1,4 +0,0 @@
-
-read -p 'Font size: ' font_size
-
-gsettings set org.gnome.desktop.interface text-scaling-factor $font_size
---
+++ disk.sh
@@ -0,0 +1,5 @@
+# https://phoenixnap.com/kb/linux-format-disk
+
+# curl -sSL https://raw.githubusercontent.com/nextcloud/nextcloudpi/master/install.sh | bash
+
+https://www.youtube.com/watch?v=5aGKWkGTmAw&list=WL&index=17&t=1195s
