2ee60208f47377f3aa50a1c59e0c8fd47f402de8
Zefeng Yao <36053374+yao1999@users.noreply.github.com>
2022-05-20 20:04:51 -0400

    new

--- nextcloud.sh
+++ nextcloud.sh
@@ -1,5 +1,13 @@
 # https://phoenixnap.com/kb/linux-format-disk
 
-# curl -sSL https://raw.githubusercontent.com/nextcloud/nextcloudpi/master/install.sh | bash
+# https://wiredzero.com/tutorials/how-to-install-nextcloud-on-a-raspberry-pi-and-add-external-storage-and-secure-with-ssl
 
-https://www.youtube.com/watch?v=5aGKWkGTmAw&list=WL&index=17&t=1195s
+https://www.youtube.com/watch?v=yJBM9of4KBQ
+
+Admin:
+admin
+&9a9s9d9A
+
+
+Disk01
+UUID=74551BF065483787 /media/disk01 auto defaults,uid=33,gid=33 0 2
