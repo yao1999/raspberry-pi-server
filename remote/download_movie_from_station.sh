f13bcf91fb1a7035c2f1afd986c2e778ec54859d
Zefeng Yao <36053374+yao1999@users.noreply.github.com>
2022-06-21 15:26:58 -0400

    new

--- remote/download_movie_from_station.sh
+++ remote/download_movie_from_station.sh
@@ -7,21 +7,21 @@ read -p 'movie-station: ' MS_number
 
 if [ $MS_number -eq 1 ]
 then
-    rsync -avcn movie-station-$MS_number:/home/ubuntu/download/* /media/disk01/Movie | grep -v '/$'
+    rsync -avcn movie-station-$MS_number:/home/ubuntu/download/* /media/disk01 | grep -v '/$'
     read -p 'download from movie-station-$MS_number? (y/n)' user_choise
     if [ $user_choise = "y" ]
     then
-        rsync -av movie-station-$MS_number:/home/ubuntu/download/* /media/disk01/Movie
+        rsync -av --ignore-existing movie-station-$MS_number:/home/ubuntu/download/* /media/disk01
     else
         echo "invalid input"
     fi
 elif [ $MS_number -eq 2 ]
 then
-    rsync -rvcn movie-station-$MS_number:/home/ubuntu/temp/* /home/pi/temp | grep -v '/$'
+    rsync -rvcn movie-station-$MS_number:/home/ubuntu/download/* /media/disk01 | grep -v '/$'
     read -p 'download from movie-station-$MS_number? (y/n)' user_choise
     if [ $user_choise = "y" ]
     then
-        rsync -av --ignore-existing movie-station-$MS_number:/home/ubuntu/download/* /media/disk01/Movie
+        rsync -av --ignore-existing movie-station-$MS_number:/home/ubuntu/download/* /media/disk01
     else
         echo "invalid input"
     fi
--- remote/virtual_machine_connect.sh
+++ remote/virtual_machine_connect.sh
@@ -3,6 +3,7 @@
 echo "1. virtual-machine-1"
 echo "2. virtual-machine-2"
 echo "3. virtual-machine-3"
+echo "4. file-storage-3"
 
 read -p 'virtual-machine: ' VM_number
 
@@ -15,6 +16,9 @@ then
 elif [ $VM_number -eq 3 ]
 then
     ssh virtual-machine-$VM_number
+elif [ $VM_number -eq 4 ]
+then
+    ssh file-storage-3
 else
     echo "No such virtual-machine"
 fi
